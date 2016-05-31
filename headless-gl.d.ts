a01e89380a0192b37e7e027a564618dd67ef83f4
LimeStreem <LimeStreem@gmail.com>
2016-05-31 14:16:59 +0900

    fix for typings

--- headless-gl.d.ts
+++ headless-gl.d.ts
@@ -1,27 +1,27 @@
-declare module "gl" {
 
-    interface STACKGL_resize_drawingbuffer {
-        resize(width: GLint, height: GLint): void;
-    }
-
-    interface STACKGL_destroy_context {
-        destroy(): void;
-    }
-
-    interface HeadlessGLOptions {
-        alpha?: boolean;
-        depth?: boolean;
-        stencil?: boolean;
-        premultipliedAlpha?: boolean;
-        preserveDrawingBuffer?: boolean;
-        preferLowPowerToHighPerformance?: boolean;
-        failIfMajorPerformanceCaveat?: boolean;
-    }
-
-    interface HeadlessWebGLRenderingContext extends WebGLRenderingContext {
-        getExtension(extName: "STACKGL_resize_drawingbuffer"): STACKGL_resize_drawingbuffer;
-        getExtension(extName: "STACKGL_destroy_context"): STACKGL_destroy_context;
-    }
-
-    export default function(width: number, height: number, options?: HeadlessGLOptions): HeadlessWebGLRenderingContext;
+interface STACKGL_resize_drawingbuffer {
+    resize(width: GLint, height: GLint): void;
 }
+
+interface STACKGL_destroy_context {
+    destroy(): void;
+}
+
+interface HeadlessGLOptions {
+    alpha?: boolean;
+    depth?: boolean;
+    stencil?: boolean;
+    premultipliedAlpha?: boolean;
+    preserveDrawingBuffer?: boolean;
+    preferLowPowerToHighPerformance?: boolean;
+    failIfMajorPerformanceCaveat?: boolean;
+}
+
+interface HeadlessWebGLRenderingContext extends WebGLRenderingContext {
+    getExtension(extName: "STACKGL_resize_drawingbuffer"): STACKGL_resize_drawingbuffer;
+    getExtension(extName: "STACKGL_destroy_context"): STACKGL_destroy_context;
+}
+
+declare function gl(width: number, height: number, options?: HeadlessGLOptions): HeadlessWebGLRenderingContext;
+
+export = gl;
